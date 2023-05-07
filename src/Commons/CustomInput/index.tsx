1fa1a07712ea7d162fca7ed4861855bca92d4637
MaryTreasure <mari1991918@gmail.com>
2023-05-07 22:37:45 +0300

    add form state

--- src/Commons/CustomInput/index.tsx
+++ src/Commons/CustomInput/index.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 
 interface Iprops {
-  value: string;
-  fieldLabel: string;
-  fieldName: string;
-  type?: React.HTMLInputTypeAttribute;
+  value: string
+  fieldLabel: string
+  fieldName: string
+  type?: React.HTMLInputTypeAttribute
+  handleChangeFieldForm: (fieldName: string) => (e: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-export const CustomInput = ({ value, fieldLabel, fieldName, type }: Iprops) => {
+export const CustomInput = ({ value, fieldLabel, fieldName, type, handleChangeFieldForm }: Iprops) => {
   return (
-    <div>
+    <div style={{ display: 'grid' }}>
       <span>{fieldLabel}</span>
-      <input value={value} type={type} />
+      <input value={value} type={type} onChange={handleChangeFieldForm(fieldName)}/>
     </div>
   );
 };
--- src/Components/Auth/AuthForm/index.tsx
+++ src/Components/Auth/AuthForm/index.tsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { CustomInput } from '../../../Commons/CustomInput';
 
 export const AuthForm = () => {
-  return <div>
-    <CustomInput />
-    <CustomInput />
-  </div>;
+  const [formState, setFormState] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChangeFieldForm = (fieldName: string) => (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormState((prevValues) => ({ ...prevValues, [fieldName]: e.target.value }));
+  };
+  return (
+    <div>
+      <CustomInput
+        fieldLabel="Name"
+        fieldName="name"
+        value={formState.name}
+        handleChangeFieldForm={handleChangeFieldForm}
+      />
+      <CustomInput
+        fieldLabel="Email"
+        fieldName="email"
+        value={formState.email}
+        handleChangeFieldForm={handleChangeFieldForm}
+      />
+      <CustomInput
+        fieldLabel="Password"
+        fieldName="password"
+        value={formState.password}
+        type="password"
+        handleChangeFieldForm={handleChangeFieldForm}
+      />
+    </div>
+  );
 };
