6925b66a1c06d03188162a3e085302679f5fefe5
MaryTreasure <mari1991918@gmail.com>
2023-06-26 22:05:14 +0300

    add MarsRovers page

--- src/Components/MarsRovers/index.tsx
+++ src/Components/MarsRovers/index.tsx
@@ -1,14 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { routes } from '../../utils/constants/routes';
+import styles from './MarsRovers.module.scss';
+import rover from '../../images/marsRovers/rover.jpg';
 
 export const MarsRovers = () => {
   return (
     <>
-      <Link to={routes.MAST}>MAST rover</Link>
-      <Link to={routes.CHEMCAM}>Chemistry and Camera Complex</Link>
-      <Link to={routes.FHAZ}>Front Hazard Avoidance Camera</Link>
-      <Link to={routes.MAHLI}>Mars Hand Lens Imager</Link>
+      <h1 className={styles.heading}>Mars rovers photo</h1>
+      <div className={styles.mainBlock}>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. In alias vero temporibus
+          voluptatum voluptates perferendis iusto quos accusantium ad iure, accusamus optio autem
+          id, at porro inventore blanditiis? Dolores, laudantium?
+        </p>
+        <div className={styles.poster}>
+          <img src={rover} alt="rover" />
+        </div>
+      </div>
+      <h2 className={styles.listHeading}>Choose a camera to see Mars</h2>
+      <ul className={styles.list}>
+        <li className={styles.listItem}>
+          <Link to={routes.MAST}>Mast Camera</Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link to={routes.CHEMCAM}>Chemistry and Camera Complex</Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link to={routes.FHAZ}>Front Hazard Avoidance Camera</Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link to={routes.MAHLI}>Mars Hand Lens Imager</Link>
+        </li>
+      </ul>
     </>
   );
 };
